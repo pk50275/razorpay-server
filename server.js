3b8c6a6591d980c9fc8c3fb7cc4df6321bf64dc8
Pawan Kumar <pk50275@gmail.com>
2026-04-20 15:29:03 +0530

    added payment verification

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
+const crypto = require("crypto");
 
 const app = express();
 app.use(cors());
@@ -30,6 +31,35 @@ app.post("/create-order", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Error creating order");
+  }
+});
+
+app.post("/verify-payment", (req, res) => {
+  try {
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+    } = req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.KEY_SECRET)
+      .update(body.toString())
+      .digest("hex");
+
+    if (expectedSignature === razorpay_signature) {
+      res.json({ success: true });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Invalid signature",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Verification failed" });
   }
 });
 
